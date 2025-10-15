440417776b6d0fb2d9591c61054f8850f5a1762c
Rusz Baidhya <ruszbaidhya@gmail.com>
2025-10-15 11:07:10 +0100

    Github_Login: Adding github login

--- src/pages/login/otherLogin/Github_Login.jsx
+++ src/pages/login/otherLogin/Github_Login.jsx
@@ -1,18 +1,30 @@
 import React from 'react'
 import { Border_Light } from '../../../components'
 import github from "../../../assets/company_logos/github.svg";
+import { GithubAuthProvider, signInWithPopup } from 'firebase/auth';
+import { auth } from '../../../firebase/firebase';
 
 function Github_Login() {
-   return (
-      <div className='mb-8 cursor-pointer hover:scale-105 transition-transform duration-200'>
-         <Border_Light className='!p-0 !rounded-xl'>
+  const githubProvider = new GithubAuthProvider();
+
+  const handleLogin = async () => {
+    try {
+      await signInWithPopup(auth, githubProvider);
+    } catch (error) {
+      console.error("Error signing in with GitHub:", error);
+    }
+  };
+
+  return (
+    <button className='mb-8 cursor-pointer hover:scale-105 transition-transform duration-200' onClick={handleLogin}>
+      <Border_Light className='!p-0 !rounded-xl'>
             <div className="flex gap-3 pl-1">
                <img src={github} alt="Github logo" className="w-6" />
 
             </div>
          </Border_Light>
 
-      </div>
+      </button>
    )
 }
 
