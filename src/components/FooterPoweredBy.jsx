bdbf0006986fd3202068fbadd711a52c7d76f2e7
Rusz Baidhya <ruszbaidhya@gmail.com>
2025-11-20 09:28:55 +0000

    Adding Tag in Footer | ChatHome_Right: Fixing Streaming Chat Logo

---
+++ src/components/FooterPoweredBy.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function FooterPoweredBy() {
+   return (
+      <div className="flex flex-row items-center justify-center gap-1 mt-1">
+         <a href=""><img src="/Lumo Studio White No BG.png" alt="Lumo Studio Logo" className='w-15' /></a>
+         <p>by <span className='font-bold'><a href="ruszbaidhya.com">Rusz .</a></span></p>
+      </div>
+  )
+}
+
+export default FooterPoweredBy
