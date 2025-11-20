416af3eb038892b4b64432b81ee7d906b5600ce9
Rusz Baidhya <ruszbaidhya@gmail.com>
2025-11-20 09:40:38 +0000

    FooterPoweredBy: Fixing Powered By Link:

--- src/components/FooterPoweredBy.jsx
+++ src/components/FooterPoweredBy.jsx
@@ -4,7 +4,7 @@ function FooterPoweredBy() {
    return (
       <div className="flex flex-row items-center justify-center gap-1 mt-1">
          <a href=""><img src="/Lumo Studio White No BG.png" alt="Lumo Studio Logo" className='w-15' /></a>
-         <p>by <span className='font-bold'><a href="ruszbaidhya.com">Rusz .</a></span></p>
+         <p>by <span className='font-bold'><a href="https://ruszbaidhya.com" target='_blank'>Rusz .</a></span></p>
       </div>
   )
 }
